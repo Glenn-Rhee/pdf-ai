f530cda1e9ab47fda6794554e560ee0523ae14ea
Glenn-Rhee <glennviktor5@gmail.com>
2026-02-06 21:04:17 +0700

    refactor: add more condition if user doesn't excist at db

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,4 @@
+import { prisma } from "@/lib/prisma";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { redirect } from "next/navigation";
 
@@ -5,6 +6,9 @@ export default async function DashboardPage() {
   const { getUser } = getKindeServerSession();
   const user = await getUser();
   if (!user || !user.id) redirect("/auth-callback?origin=dashboard");
+
+  const dataUser = await prisma.user.findFirst({ where: { id: user.id } });
+  if (!dataUser) redirect("/auth-callback?origin=dashboard");
 
   return <div>{user?.email}</div>;
 }
