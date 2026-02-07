d8c97e4ff6c98ce7c1dc0ce1f558284e9ac85d3e
Glenn-Rhee <glennviktor5@gmail.com>
2026-02-07 20:23:41 +0700

    feat: added model File

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -7,6 +7,9 @@ export default function Dashboard() {
         <h1 className="mb-3 font-bold text-5xl text-gray-900">My Files</h1>
         <UploadButton />
       </div>
+
+      {/* Display user files */}
+      
     </main>
   );
 }
