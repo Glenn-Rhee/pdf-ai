6f38774ae8c4ddbf1a0c85b6b71a533060f52998
Glenn-Rhee <glennviktor5@gmail.com>
2026-02-11 18:20:32 +0700

    feat: added custom ui while loading, error, and onLoadError

--- src/components/PdfRenderer.tsx
+++ src/components/PdfRenderer.tsx
@@ -3,6 +3,8 @@ import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/Page/AnnotationLayer.css";
 import "react-pdf/dist/Page/TextLayer.css";
 import { PdfRendererProps } from "./WrapperPdf";
+import { Loader2 } from "lucide-react";
+import toast from "react-hot-toast";
 
 pdfjs.GlobalWorkerOptions.workerSrc = new URL(
   "pdfjs-dist/build/pdf.worker.min.mjs",
@@ -19,7 +21,25 @@ export default function PdfRenderer(props: PdfRendererProps) {
 
       <div className="flex-1 w-full max-h-screen">
         <div>
-          <Document file={url} className={"max-h-full"}>
+          <Document
+            file={url}
+            loading={
+              <div className="flex justify-center">
+                <Loader2 className="my-24 h-6 w-6 animate-spin" />
+              </div>
+            }
+            onLoadError={() => {
+              toast.error("Error load pdf! Please try again later.");
+            }}
+            error={
+              <div className="flex justify-center">
+                <p className="font-bold text-red-500 text-3xl my-20 text-center">
+                  Error load pdf! Please try again later.
+                </p>
+              </div>
+            }
+            className={"max-h-full"}
+          >
             <Page pageNumber={1} />
           </Document>
         </div>
