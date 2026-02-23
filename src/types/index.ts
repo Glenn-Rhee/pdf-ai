f4bfc838b77566ee02e9e5142af899ca6eb5809c
Glenn-Rhee <glennviktor5@gmail.com>
2026-02-23 20:40:14 +0700

    feat: added type dynamiic of message

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,6 +1,19 @@
+import { inferRouterOutputs } from "@trpc/server";
+import { AppRouter } from "../trpc";
+import { JSX } from "react";
+
 export interface ResponsePayload<T = unknown> {
   status: "success" | "failed";
   message: string;
   code: number;
   data: T;
 }
+
+type RouterOutput = inferRouterOutputs<AppRouter>;
+type Message = RouterOutput["getFileMessages"]["messages"];
+type OmitText = Omit<Message[number], "text">;
+type ExtendedText = {
+  text: string | JSX.Element;
+};
+
+export type ExtendedMessage = OmitText & ExtendedText;
