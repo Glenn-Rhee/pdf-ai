a8b3d35d1b2c397cfa0788d030d09940d059a4bb
Glenn-Rhee <glennviktor5@gmail.com>
2026-02-23 20:51:32 +0700

    feat: added message from user and AI

--- src/components/chat/Message.tsx
+++ src/components/chat/Message.tsx
@@ -1,6 +1,8 @@
 import { cn } from "@/lib/utils";
 import { ExtendedMessage } from "@/src/types";
 import { Icons } from "../Icons";
+import ReactMarkdown from "react-markdown";
+import { format } from "date-fns";
 
 interface MessageProps {
   isNextMessageSamePerson: boolean;
@@ -30,6 +32,40 @@ export default function Message(props: MessageProps) {
         ) : (
           <Icons.logo className="fill-zinc-300 h-3/4 w-3/4" />
         )}
+      </div>
+
+      <div
+        className={cn("flex flex-col space-y-2 text-base max-w-md mx-2", {
+          "order-1 items-end": message.isUserMessage,
+          "order-1 items-start": !message.isUserMessage,
+        })}
+      >
+        <div
+          className={cn("px-4 py-2 rounded-lg inline-block", {
+            "bg-orange-600 text-white": message.isUserMessage,
+            "bg-gray-200 text-gray-900": !message.isUserMessage,
+            "rounded-br-none":
+              !isNextMessageSamePerson && message.isUserMessage,
+            "rounded-bl-none":
+              !isNextMessageSamePerson && !message.isUserMessage,
+          })}
+        >
+          {typeof message.text === "string" ? (
+            <ReactMarkdown>{message.text}</ReactMarkdown>
+          ) : (
+            message.text
+          )}
+          {message.id !== "loading-message" ? (
+            <div
+              className={cn("text-xs select-none mt-2 w-full text-right", {
+                "text-zinc-500": !message.isUserMessage,
+                "text-orange-300": message.isUserMessage,
+              })}
+            >
+              {format(new Date(message.createdAt), "HH:mm")}
+            </div>
+          ) : null}
+        </div>
       </div>
     </div>
   );
