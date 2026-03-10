faab474b44162fef06fb792e0752895a9562bd7f
Glenn-Rhee <glennviktor5@gmail.com>
2026-03-10 21:27:37 +0700

    refactor: update HeaderPricing

---
+++ src/components/HeaderPricing.tsx
@@ -0,0 +1,11 @@
+export default function HeaderPricing() {
+  return (
+    <div className="mx-auto mb-10 sm:max-w-lg">
+      <h1 className="text-6xl font-bold sm:text-7xl">Pricing</h1>
+      <p className="mt-5 text-gray-600 sm:text-lg">
+        Whether you&apos;re just trying out our service or need more,
+        we&apos;vew got you covered.
+      </p>
+    </div>
+  );
+}
