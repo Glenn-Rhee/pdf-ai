e269e58c3bdc9aa8b9a44f16b37d78dbe1b567cd
Glenn-Rhee <glennviktor5@gmail.com>
2026-03-10 21:28:33 +0700

    refactor: delete pricingItems

--- app/pricing/page.tsx
+++ app/pricing/page.tsx
@@ -14,61 +14,6 @@ import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { ArrowRight, Check, HelpCircle, Minus } from "lucide-react";
 import Link from "next/link";
 
-const pricingItems = [
-  {
-    plan: "Free",
-    tagline: "For small side projects.",
-    quota: 10,
-    features: [
-      {
-        text: "5 pages per PDF",
-        footnote: "The maximum amount of pages per PDF-file.",
-      },
-      {
-        text: "4MB file size limit",
-        footnote: "The maximum file size of a single PDF file.",
-      },
-      {
-        text: "Mobile-friendly interface",
-      },
-      {
-        text: "Higher-quality responses",
-        footnote: "Better algorithmic responses for enhanced content quality",
-        negative: true,
-      },
-      {
-        text: "Priority support",
-        negative: true,
-      },
-    ],
-  },
-  {
-    plan: "Pro",
-    tagline: "For larger projects with higher needs.",
-    quota: PLANS.find((p) => p.slug === "pro")!.quota,
-    features: [
-      {
-        text: "25 pages per PDF",
-        footnote: "The maximum amount of pages per PDF-file.",
-      },
-      {
-        text: "16MB file size limit",
-        footnote: "The maximum file size of a single PDF file.",
-      },
-      {
-        text: "Mobile-friendly interface",
-      },
-      {
-        text: "Higher-quality responses",
-        footnote: "Better algorithmic responses for enhanced content quality",
-      },
-      {
-        text: "Priority support",
-      },
-    ],
-  },
-];
-
 export default async function PricingPage() {
   const { getUser } = getKindeServerSession();
   const user = await getUser();
