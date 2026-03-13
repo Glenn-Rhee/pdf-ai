dfdede06a80d6f829244b7306a117c2f135e1b47
Glenn-Rhee <glennviktor5@gmail.com>
2026-03-13 20:57:56 +0700

    feat: added useMutation at upgrade button

--- src/components/UpgradeButton.tsx
+++ src/components/UpgradeButton.tsx
@@ -1,10 +1,17 @@
 "use client";
+import { trpc } from "@/app/_trpc/client";
 import { Button } from "@/components/ui/button";
 import { ArrowRight } from "lucide-react";
 
 export default function UpgradeButton() {
+  const {mutate: createStripeSession} = trpc.createStripeSession.useMutation({
+    onSuccess: ({ url }) => {
+      window.location.href = url ?? "/dashboard/billing";
+    },
+  });
+  
   return (
-    <Button>
+    <Button onClick={() => createStripeSession()}>
       Upgrade Now <ArrowRight className="h-5 w-5 ml-1.5" />
     </Button>
   );
